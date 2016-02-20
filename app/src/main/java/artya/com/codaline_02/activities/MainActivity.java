c9ea58736e8367b116a83849f57760806fda76e0
Artem Merezhko <cloxteam@gmail.com>
2016-02-20 16:33:55 +0200

    Homework 3

--- app/src/main/java/artya/com/codaline_02/activities/MainActivity.java
+++ app/src/main/java/artya/com/codaline_02/activities/MainActivity.java
@@ -1,8 +1,13 @@
 package artya.com.codaline_02.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewCompat;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,19 +18,32 @@ import artya.com.codaline_02.R;
  * Created by artya on 13.02.16.
  */
 public class MainActivity extends FragmentActivity{
+
+    SharedPreferences sharedpreferences;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Name = "nameKey";
+    SharedPreferences.Editor editor;
+    String n;
+    EditText fieldForInput;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+
     }
 
     //Counting symbols
     public void onClickCalculate(View view){
-        EditText fieldForInput = (EditText) findViewById(R.id.field_for_input);
+        fieldForInput = (EditText) findViewById(R.id.field_for_input);
         TextView txtCountOfSymbols = (TextView) findViewById(R.id.txt_count_of_symbols);
 
         //next line is not optimal and I know it :)
-        txtCountOfSymbols.setText("Total count of symbols: " + String.valueOf(fieldForInput.getText().toString().length()));
+        txtCountOfSymbols.setText(String.valueOf(fieldForInput.getText().toString().length()));
+
     }
 
     //processing BACK key
@@ -36,8 +54,36 @@ public class MainActivity extends FragmentActivity{
         if (back_pressed + 2000 > System.currentTimeMillis())
             super.onBackPressed();
         else
-            Toast.makeText(getBaseContext(), "Press once again to exit!",
+            Toast.makeText(getBaseContext(), R.string.exit_toast,
                     Toast.LENGTH_SHORT).show();
         back_pressed = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onDestroy() {
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        n = fieldForInput.getText().toString();
+        editor = sharedpreferences.edit();
+        editor.putString(Name, n).apply();
+        editor.commit();
+        Toast.makeText(MainActivity.this,"Text saved",Toast.LENGTH_LONG).show();
+        super.onDestroy();
+    }
+
+
+    // не виходить отримати збережений рядок
+    @Override
+    protected void onStart() {
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+
+        // не  виходить прив’язати fieldForInput = sharedpreferences.getString(MyPREFERENCES, n);
+        //не знайю, що ротиби тут
+        sharedpreferences.getString(MyPREFERENCES, n);
+
+
+        //fieldForInput.setText(String.valueOf(sharedpreferences)); - вилітає exeption
+
+        super.onStart();
     }
 }
